abcb1610527705fb9f043fc908e02ceeff9ebdfd
Maggie Wiseman <maggie.wiseman@gmail.com>
2018-12-07 11:22:52 -0500

    initial commit

---
+++ CalebCurrySignalRTutorial/MyHub.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace CalebCurrySignalRTutorial
+{
+    public class MyHub : Hub
+    {
+        public void Announce(string message)
+        {
+            //when this method gets called we call the announce method on all the clients. 
+            Clients.All.Announce(message);
+        }
+    }
+}
---
+++ CalebCurrySignalRTutorial/Scripts/SignalR.js
@@ -0,0 +1,23 @@
+﻿
+
+(function ()
+{
+    $.connection.hub.start()
+        .done(function ()
+        {
+            console.log("It worked!");
+            $.connection.myHub.server.announce("Connected");
+        })
+        .fail(function ()
+        {
+            alert("FAIL!!!!")
+        });
+
+
+    $.connection.myHub.client.announce = function (message)
+    {
+        $("#welcome-messages").html(message)
+    }
+
+
+})();
